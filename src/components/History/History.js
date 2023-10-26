f0df313ce08e24307f2225a8de93d6027729a027
Deepak Kumar D <deepak.kumard@saveo.in>
2023-10-27 00:58:37 +0530

    history page created

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -2,5 +2,39 @@ import React from "react";
 import "./History.css";
 
 export default function History() {
-  return <div>History</div>;
+  return (
+    <section>
+      <img src="/images/history/History-Header.jpg" className="menu-header" />
+
+      <div className="history-cntr">
+        <div className="history-img">
+          <img src="/images/history/Img1.png" alt="Img1" />
+        </div>
+
+        <div className="history-info">
+          <p>Mother's Touch Kafe commenced its journey with a heartfelt aspiration: to radiate love and affection through the medium of culinary artistry. Our story began in the year 2020 amidst the bustling streets of Bangalore, igniting an odyssey that has led us to touch the lives and palates of countless individuals. What once started as a modest venture has now matured into a culinary oasis, embraced by a community of over 150 patrons who have found solace in our offerings.</p>
+        </div>
+      </div>
+
+      <div className="history-cntr">
+        <div className="history-info">
+          <p>Beyond numbers, it's the shared moments of joy that define our legacy. With a personal touch, we've had the privilege of serving and sharing in the happiness of over 2000 patrons, etching unforgettable memories. Notably, our cakes have graced the tables of 100 discerning customers, infusing sweetness into their special moments. From intimate gatherings to opulent wedding affairs, Mother's Touch Kafe has emerged as the go-to choice for those seeking to weave flavor into their cherished events.</p>
+        </div>
+
+        <div className="history-img">
+          <img src="/images/history/Img2.png" alt="Img1" />
+        </div>
+      </div>
+
+      <div className="history-cntr">
+        <div className="history-img">
+          <img src="/images/history/Img3.png" alt="Img1" />
+        </div>
+
+        <div className="history-info">
+          <p>As we embrace innovation, our newly launched website embodies our dedication to excellence, authenticity, and the artistry of culinary expression. Each creation from our kitchen encapsulates not only the richness of taste but also the affectionate touch that defines a Mother's Touch. We invite you to embark on a journey of taste, tradition, and unity, where each delectable bite symbolizes an opportunity to nourish not just the body, but also the spirit.</p>
+        </div>
+      </div>
+    </section>
+  );
 }
