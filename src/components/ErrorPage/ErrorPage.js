d3001fef7d5138159a2312caf52d4fd7346e9ea6
Deepak Kumar D <deepak.kumard@saveo.in>
2023-11-07 19:01:32 +0530

    errorpage logo updated

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./ErrorPage.css";
-import LogoFull from "../../MTF-Text-Primary-Logo_White.svg";
 import { useNavigate } from "react-router-dom";
+import LogoLoader from "../../logo.png";
 
 export default function ErrorPage() {
   const navigate = useNavigate();
   return (
     <div className="error-main-cntr">
       <div className="error-cntr">
-        <img src={LogoFull} alt="logo" />
+        <div className="loader-img-cntr">
+          <img src={LogoLoader} alt="logo loader" />
+        </div>
         <h1> 404 Page Not Found</h1>
         <button onClick={() => navigate("/")}>Go to Homepage</button>
       </div>
